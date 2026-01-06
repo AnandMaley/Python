20f207ba493a3ac6b7197e3b9371d37d881360d5
Anand G Maley <maleyanand@gmail.com>
2026-01-06 19:21:58 +0530

    commit

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
- # Storing tasks
-tasks = {}
+def save_tasks(tasks):
+    with open("D:\Projects\Python\\tasks.txt","w") as file:
+        file.write(str(tasks))
 
 def load_tasks():
-    global tasks
-    print("tasks_id|tasks|status")
-    for tasks_id,(tn,status) in tasks.items():
-        print(f"{tasks_id}|{tn}|{status}")
-    # with open("D:\Projects\Python\\tasks.txt","r") as file:
-    #     x = file.read()
-    #     print(x)
+    # Storing tasks
+    with open("D:\Projects\Python\\tasks.txt","r") as file:
+        tasks = file.read()
+        return dict(tasks)
 
-def add_tasks():
+def list_tasks(tasks):
+    print("tasks_id|tasks|status")
+    for tasks_id,(ts,status) in tasks.items():
+        print(f"{tasks_id}|{ts}|{status}")
+
+def add_tasks(tasks):
     task_add = input("Enter the Task: ")
     tasks.update({len(tasks):[task_add,"Not Done"]})
-    # with open("D:\Projects\Python\\tasks.txt","w") as file:
-    #     file.write(str(tasks))
 
-def delete_task():
-    load_tasks()
+def delete_task(tasks):
     id = int(input("Enter the Id of the Task you want to DELETE: "))
-    tasks.pop(id) 
-    # with open("D:\Projects\Python\\tasks.txt","w") as file:
-    #     file.write(str(tasks))
+    tasks.pop(id)
 
-def update_task():
-    load_tasks()
+def update_task(tasks):
+    list_tasks(tasks)
     id = int(input("Enter the Id of the Task you want to UPDATE: "))
     upts = tasks.get(id)
     col = int(input("Enter 1 for Updating Task or 2 for Updating Status :"))
@@ -33,10 +31,10 @@ def update_task():
     if col == 2:
         upts[col-1] = input("Enter the Status :")
     tasks.update({id:[upts[0],upts[1]]})
-    # with open("D:\Projects\Python\\tasks.txt","w") as file:
-    #     file.write(str(tasks))
+    save_tasks(tasks)
 
 def main():
+    tasks = load_tasks()
     while True:
         print("Enter the Number and Press ENTER to perform the task:\n" \
         "1. Add Task\n"
@@ -48,14 +46,15 @@ def main():
         if choice >=1 and choice <= 5:
             print(f"You have selected option {choice}.")
             if choice == 1:
-                add_tasks()
+                add_tasks(tasks)
             elif choice == 2:
-                load_tasks()
+                list_tasks(tasks)
             elif choice == 3:
-                delete_task()
+                delete_task(tasks)
             elif choice == 4:
-                update_task()
+                update_task(tasks)
             else:
+                save_tasks(tasks)
                 break
         else:
             print("Invalid option selected. Please try again.")
